8bf65704d84cf0e32c28b6747efa712f0a0ef3df
LAKSH-XD <127467419+LAKSH-XD@users.noreply.github.com>
2023-03-10 12:30:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "6117249994").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/NomoreLakshya/Sanki-Music")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/LAKSH-XD/Sanki-Music")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
